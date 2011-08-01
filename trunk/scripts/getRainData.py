38d443293fe5af1716369ea9b166851ce68500f7
dan@inlet.geol.sc.edu <dan@inlet.geol.sc.edu@347c21a6-de56-11dd-a37d-c9d1c40e0883>
2011-08-01 15:13:59 +0000

    Date: 2011-07-27 Function: vacuum Changes: Use the dhecXMRGProcessing object to vacuum the database instead of the processDHECRainGauges object.   processDHECRainGauges is being deprecated.

--- trunk/scripts/getRainData.py
+++ trunk/scripts/getRainData.py
@@ -1,7 +1,14 @@
+"""
+Revisions
+Date: 2011-07-27
+Function: vacuum
+Changes: Use the dhecXMRGProcessing object to vacuum the database instead of the processDHECRainGauges object.
+  processDHECRainGauges is being deprecated.
+"""
 import sys
 import optparse
-
-#sys.path.append("C:\Documents and Settings\dramage\workspace\BeachAdvisory") 
+import time
+sys.path.append("C:\Documents and Settings\dramage\workspace\BeachAdvisory") 
 from dhecRainGaugeProcessing import dhecDB
 from dhecRainGaugeProcessing import rainGaugeData
 from dhecRainGaugeProcessing import processDHECRainGauges 
@@ -13,8 +20,8 @@ from dhecXMRGProcessing import dhecXMRGProcessing
 
 def getRainGaugeData(configFile):
   dhecData = processDHECRainGauges(configFile)
-  #dhecData.deleteRainGaugeDataFiles()
-  #dhecData.ftpRainGaugeData()
+#  dhecData.deleteRainGaugeDataFiles()
+#  dhecData.ftpRainGaugeData()
   dhecData.processFiles()
   #Check to make sure data flowed.
   #dhecData.checkDataFlow()
@@ -24,9 +31,150 @@ def getRainGaugeData(configFile):
 def getXMRGData(configFile):
   xmrgData = dhecXMRGProcessing(configFile)
   xmrgData.getLatestHourXMRGData()
+
+def createXMRGSummaryFiles(configFile):
   
+  try:
+    
+    xmrgData = dhecXMRGProcessing(configFile)
+    db = dhecDB(xmrgData.configSettings.dbSettings['dbName'], xmrgData.configSettings.loggerName)
+    if(db.logger != None):
+      db.logger.debug("Beginning createXMRGSummaryFiles");
+    else:
+      print("Beginning createXMRGSummaryFiles")      
+    outputDirectory = xmrgData.configSettings.getEntry('//xmrgData/processingSettings/summaryDirectory')
+    if(outputDirectory == None or len(outputDirectory) == 0):
+      if(db.logger != None):
+        db.logger.error("No output directory given in config file for //xmrgData/processingSettings/summaryDirectory. Cannot continue.")
+      else:
+        print("No output directory given in config file for //xmrgData/processingSettings/summaryDirectory. Cannot continue.")      
+          
+    #Starting date is yesterday at 12am. We take the current date and subtract a day, then set time to 12am.
+    dateTime = (time.mktime(time.localtime()) - (24 * 3600))
+    dateTime = time.localtime(dateTime)
+    #now set the hour,min,second to 12am(00:00:00)
+    dateTime = time.struct_time((dateTime[0],dateTime[1],dateTime[2],0,0,0,0,0,0))
+    #Now finally let's get the UTC time.
+    localTime = time.strftime("%Y-%m-%d_%H-%M-%S", dateTime)
+    dateTime = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(time.mktime(dateTime)))
+    rgCursor = db.getRainGauges();
+    for row in rgCursor:    
+      platformHandle = row['platform_handle']
+      shortName = row['short_name']
+      if(db.logger != None):
+        db.logger.debug("Processing %s" %(platformHandle));
+      else:
+        print("Processing %s" %(platformHandle))      
+
+      #Open the output file.
+      fileName = "%s/%s-%s.csv" %(outputDirectory, shortName,localTime)
+      if(db.logger != None):
+        db.logger.debug("Opening output file %s" %(fileName));
+      else:
+        print("Opening output file %s" %(fileName))      
+      outFile = open(fileName, "w")
+
+      xmrgData = db.createXMRGStats(dateTime, shortName)
+      #Loop through and convert values to strings.
+      for key in xmrgData:
+        xmrgData[key] = ("" if xmrgData[key] == None else str(xmrgData[key])) 
+      outbuf = ''
+      header = '';
+      if(shortName == 'nmb1'):
+        header = ''
+        outbuf = ''
+      
+      elif(shortName == 'nmb2'):
+        header = 'Sum past 6 days rainfall'
+        outbuf = xmrgData['radarSum144']
+      
+      elif(shortName == 'nmb3'):
+        header = "Yesterday's rainfall,Intensity of yesterday's rain,Sum past 2 days rainfall,Sum past 6 days rainfall"
+        outbuf = "%s,%s,%s,%s" %\
+        (xmrgData['radarSum24'],
+         xmrgData['radarIntensity'],
+         xmrgData['radarSum48'],
+         xmrgData['radarSum144'])
+      
+      elif(shortName == 'mb1'):
+        header = "Yesterday's rainfall,Intensity of yesterday's rain,# Previous dry days,Two day delay rain,Sum past 2 days rainfall"
+        outbuf = "%s,%s,%s,%s,%s"\
+           % (xmrgData['radarSum24'],
+              xmrgData['radarIntensity'],
+              xmrgData['radarDryCnt'],
+              xmrgData['radarsum2daydelay'],
+              xmrgData['radarSum48'])
+      
+      elif(shortName == 'mb2'):
+        header = "Yesterday's rainfall,Intensity of yesterday's rain,# Previous dry days,One day delay rain,Sum past 6 days rainfall"
+        outbuf = "%s,%s,%s,%s,%s"\
+        % (xmrgData['radarSum24'],
+           xmrgData['radarIntensity'],
+           xmrgData['radarDryCnt'],
+           xmrgData['radarsum1daydelay'],
+           xmrgData['radarSum144'])
+      
+      elif(shortName == 'mb3'):
+        header = "Yesterday's rainfall,Intensity of yesterday's rain,One day delay rain,Sum past 3 days rainfall,Sum past 6 days rainfall,Sum past 7 days rainfall"
+        outbuf = "%s,%s,%s,%s,%s,%s"\
+        % (xmrgData['radarSum24'],
+           xmrgData['radarIntensity'],
+           xmrgData['radarsum1daydelay'],
+           xmrgData['radarSum72'],
+           xmrgData['radarSum144'],
+           xmrgData['radarSum168'])
+  
+      elif(shortName == 'mb4'):
+        header = "Yesterday's rainfall,Intensity of yesterday's rain,# Previous dry days,Two day delay rain,Three day delay rain,Sum past 2 days rainfall,Sum past 3 days rainfall,Sum past 7 days rainfall"
+        outbuf = "%s,%s,%s,%s,%s,%s,%s,%s" \
+        % (xmrgData['radarSum24'],
+           xmrgData['radarIntensity'],
+           xmrgData['radarDryCnt'],
+           xmrgData['radarsum2daydelay'],
+           xmrgData['radarsum3daydelay'],
+           xmrgData['radarSum48'],
+           xmrgData['radarSum72'],
+           xmrgData['radarSum168'])
+  
+      elif(shortName == 'surfside'):
+        header = "Yesterday's rainfall,Intensity of yesterday's rain,# Previous dry days,Two day delay rain,Sum past 2 days rainfall,Sum past 3 days rainfall,Sum past 4 days rainfall,Sum past 5 days rainfall,Sum past 6 days rainfall"
+        outbuf = "%s,%s,%s,%s,%s,%s,%s,%s,%s"\
+        %( xmrgData['radarSum24'],
+           xmrgData['radarIntensity'],
+           xmrgData['radarDryCnt'],
+           xmrgData['radarsum2daydelay'],
+           xmrgData['radarSum48'],
+           xmrgData['radarSum72'],
+           xmrgData['radarSum96'],
+           xmrgData['radarSum120'],
+           xmrgData['radarSum144'])
+        
+      elif(shortName == 'gardcty'):
+        header = "Yesterday's rainfall,Intensity of yesterday's rain,Sum past 5 days rainfall"
+        outbuf = "%s,%s,%s"\
+        %(xmrgData['radarSum24'],
+          xmrgData['radarIntensity'],
+          xmrgData['radarSum120'])
+        
+      outFile.write(header + "\n")
+      outFile.write(outbuf + "\n")
+      if(db.logger != None):
+        db.logger.debug("Closing output file %s" %(fileName));
+      else:
+        print("Closing output file %s" %(fileName))      
+      
+      outFile.close()
+  except Exception, e:
+    if(db.logger != None):
+      db.logger.critical(str(e) + ' Terminating script.', exc_info=1)
+    else:
+      print("ErrMsg: %s SQL: '%s'" % (e.args[0], sql))      
+    sys.exit(-1)
+    
+  return
+
 def vacuum(configFile):
-  dhecData = processDHECRainGauges(configFile)
+  dhecData = dhecXMRGProcessing(configFile)
   dhecData.vacuumDB()
 
 def backupData(configFile):
@@ -45,6 +193,8 @@ if __name__ == '__main__':
                     action= 'store_true', help="Use to vacuum the database to free unused space and shrink filesize." )
   parser.add_option("-b", "--BackupPrecipitation", dest="backupPrecip", action= 'store_true', 
                     help="Used to roll precipitation data out of the working database and into a backup database." )
+  parser.add_option("-f", "--CreateXMRGSummaryFiles", dest="createXMRGSummaryFiles", action= 'store_true', 
+                    help="Specifies creation of the XMRG summary files for DHEC excel sheets." )
   (options, args) = parser.parse_args()
   if( options.xmlConfigFile == None ):
     parser.print_usage()
@@ -60,6 +210,8 @@ if __name__ == '__main__':
       getXMRGData( options.xmlConfigFile )
     if( options.backupPrecip ):
       backupData( options.xmlConfigFile )
+    if(options.createXMRGSummaryFiles):
+      createXMRGSummaryFiles(options.xmlConfigFile)
     else:
       print( "No options specified. No actions taken.\n" )
 
